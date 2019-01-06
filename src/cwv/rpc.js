dc78c2c832e7b36f56ccc035907a3db5d7ca94d4
brew <brewcwv@gmail.com>
2019-01-06 17:18:48 +0800

    add quota to rpc

--- src/cwv/rpc.js
+++ src/cwv/rpc.js
@@ -48,10 +48,10 @@ class PatternMethod extends Method{
 
 }
 
-var getBlockByNumber = PatternMethod._(_.template('{"number":<%- args[0] %>}'),"/bct/pbgbn.do");
-var getBalance = PatternMethod._(_.template('{"address":<%- args[0] %>}'),"act","gac");
-var getBlockByHash = PatternMethod._(_.template('{"hash":<%- args[0] %>}'),"bct","GBA");
-var getTransaction = PatternMethod._(_.template('{"hash":<%- args[0] %>}'),"TXT","GTX");
+var getBlockByNumber = PatternMethod._(_.template('{"number":"<%- args[0] %>"}'),"/bct/pbgbn.do");
+var getBalance = PatternMethod._(_.template('{"address":"<%- args[0] %>"}'),"act","gac");
+var getBlockByHash = PatternMethod._(_.template('{"hash":"<%- args[0] %>}"'),"bct","GBA");
+var getTransaction = PatternMethod._(_.template('{"hash":"<%- args[0] %>}"'),"TXT","GTX");
 var sendRawTransaction = PatternMethod._(_.template('{"transaction":<%- args %>}'),"TXT","MTX");
 
 // 		   getBlockTransactionCount,
